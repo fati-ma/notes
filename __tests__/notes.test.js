1942c333e3b5cac15770aa413512b32e355cb659
fati-ma <fatima.atiyya@yahoo.com>
2021-01-03 13:18:08 +0200

    done

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -6,10 +6,10 @@ jest.spyOn(global.console, 'log');
 describe('Note Module', () => {
   it('does nothing when execute() with invalid options', () => {
     const note = new Note();
-    note.execute({ action: 'dd', payload: 'A new note Added' });
+    note.execute({ action: 'b', payload: 'A new note Added' });
     expect(console.log).not.toHaveBeenCalled();
   });
-  it('Log out options when execute() with options', () => {
+  it('log out options when execute() with options', () => {
     const note = new Note();
     note.execute({ action: 'add', payload: 'A new note Added' });
     expect(console.log).toHaveBeenCalled();
